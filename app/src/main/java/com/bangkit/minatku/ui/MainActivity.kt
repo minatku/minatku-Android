85a4f45505a03579f8f540dd418406e35a179dd4
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-01 19:40:43 +0700

    Add logo di splashactivity.xml

--- app/src/main/java/com/bangkit/minatku/ui/MainActivity.kt
+++ app/src/main/java/com/bangkit/minatku/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bangkit.minatku
+package com.bangkit.minatku.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.minatku.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
