2cf5f8c6c483d046772bad48878e418e7604d352
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-01 22:01:49 +0700

    Add WelcomeActivity

---
+++ app/src/main/java/com/bangkit/minatku/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.minatku.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.minatku.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
