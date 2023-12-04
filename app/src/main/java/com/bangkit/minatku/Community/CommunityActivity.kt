cbe33ee8117af7ec51df6f39785cb1f24fd2a848
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-04 20:23:44 +0700

    Add Initial Community

---
+++ app/src/main/java/com/bangkit/minatku/Community/CommunityActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.minatku.Community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.minatku.R
+
+class CommunityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community)
+    }
+}
