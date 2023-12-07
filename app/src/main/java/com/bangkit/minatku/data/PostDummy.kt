97403dfea9b9a894315514e0f4246c317145f470
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-07 20:42:58 +0700

    add dummy

---
+++ app/src/main/java/com/bangkit/minatku/data/PostDummy.kt
@@ -0,0 +1,10 @@
+package com.bangkit.minatku.data
+
+data class PostDummy(
+    val username: String,
+    val nama: String,
+    val descpost: String,
+    val imgurl1: String,
+    val imgurl2: String,
+    val imgurl3: String,
+)
---
+++ app/src/main/java/com/bangkit/minatku/ui/community/CommunityAdapter.kt
@@ -0,0 +1,4 @@
+package com.bangkit.minatku.ui.community
+
+class CommunityAdapter {
+}
