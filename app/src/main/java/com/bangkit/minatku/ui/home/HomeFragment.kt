ec87c87cd9ed59bc81a0d7d670023bad17a0dd82
Zeee <brianyudhistira1@gmail.com>
2023-12-14 01:54:16 +0700

    Add Bottom NavBar
    Add Bottom navbar and mentor fragment
    change some activities to fragment (modul prem & home)

--- app/src/main/java/com/bangkit/minatku/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/minatku/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.bangkit.minatku.ui.modul.non_premium
+package com.bangkit.minatku.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,10 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bangkit.minatku.R
 
-class ModulNonPrem : Fragment() {
+class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modul_non_prem, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 }
--- app/src/main/java/com/bangkit/minatku/ui/modul/premium/ModulPremFragment.kt
+++ app/src/main/java/com/bangkit/minatku/ui/modul/premium/ModulPremFragment.kt
@@ -1,27 +1,33 @@
 package com.bangkit.minatku.ui.modul.premium
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bangkit.minatku.Data.Modul_Dummy
 import com.bangkit.minatku.R
 import com.bangkit.minatku.ui.modul.premium.detail_modul.Detail_Modul
 
-class ModulPremActivity : AppCompatActivity() {
+class ModulPremFragment : Fragment() {
     private lateinit var listAdapter: RecyclerView
     private val list = ArrayList<Modul_Dummy>()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modul_prem)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_modul_prem, container, false)
 
-        listAdapter = findViewById(R.id.list_modul)
+        listAdapter = view.findViewById(R.id.list_modul)
         listAdapter.setHasFixedSize(true)
         list.addAll(getlist())
         showRecyclerList()
-    }
 
+        return view
+    }
 
     private fun getlist(): ArrayList<Modul_Dummy>{
         val judul = resources.getStringArray(R.array.judul_dummy)
@@ -36,7 +42,7 @@ class ModulPremActivity : AppCompatActivity() {
     }
 
     private fun showRecyclerList() {
-        listAdapter.layoutManager = LinearLayoutManager(this)
+        listAdapter.layoutManager = LinearLayoutManager(requireContext())
         val listmodulAdapter = ModulAdapter(list)
         listAdapter.adapter = listmodulAdapter
 
@@ -47,10 +53,10 @@ class ModulPremActivity : AppCompatActivity() {
         })
     }
 
-    private fun showSelectedModul(modulDummy: Modul_Dummy){
-        val movedata = Intent(this, Detail_Modul::class.java)
-        movedata.putExtra(Detail_Modul.EXTRA_JUDUL,modulDummy.judul)
-        movedata.putExtra(Detail_Modul.EXTRA_TEXT,modulDummy.text)
+    private fun showSelectedModul(modulDummy: Modul_Dummy) {
+        val movedata = Intent(requireContext(), Detail_Modul::class.java)
+        movedata.putExtra(Detail_Modul.EXTRA_JUDUL, modulDummy.judul)
+        movedata.putExtra(Detail_Modul.EXTRA_TEXT, modulDummy.text)
         startActivity(movedata)
     }
 }
