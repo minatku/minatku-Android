6640ce6d6c5a407e09e231564c426da5138fa450
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-16 00:03:19 +0700

    Initial Setting Activity, Fix some bug

---
+++ app/src/main/java/com/bangkit/minatku/ui/editpassword/EditPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.minatku.ui.editpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.minatku.R
+
+class EditPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_password)
+    }
+}
