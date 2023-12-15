d90d983f4330a9e57fd18d18620a864065fb1dbc
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-15 17:51:14 +0700

    Add Activity Edit Profil

---
+++ app/src/main/java/com/bangkit/minatku/ui/editprofil/EditProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.minatku.ui.editprofil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.minatku.R
+
+class EditProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profil)
+    }
+}
