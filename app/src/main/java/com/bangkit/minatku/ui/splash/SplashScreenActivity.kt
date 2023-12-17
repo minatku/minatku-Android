6bb04eba69c313644610d9fd5fef0730c659eada
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-17 12:43:15 +0700

    Make splash screen appear first and then intent to welcome

--- app/src/main/java/com/bangkit/minatku/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/bangkit/minatku/ui/splash/SplashScreenActivity.kt
@@ -2,11 +2,26 @@ package com.bangkit.minatku.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangkit.minatku.R
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import com.bangkit.minatku.databinding.ActivitySplashScreenBinding
+import com.bangkit.minatku.ui.welcome.WelcomeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private val splashScreen = 2000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, WelcomeActivity::class.java))
+            finish()
+        }, splashScreen)
     }
 }
