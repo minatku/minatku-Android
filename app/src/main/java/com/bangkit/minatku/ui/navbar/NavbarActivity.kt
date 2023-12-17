3489777b7b93cd53a45b4e4c22d229d9dcf19793
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-17 09:20:34 +0700

    Change Some Activity to Fragment and Deploy it on Navbar

--- app/src/main/java/com/bangkit/minatku/ui/navbar/NavbarActivity.kt
+++ app/src/main/java/com/bangkit/minatku/ui/navbar/NavbarActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.bangkit.minatku.R
 import com.bangkit.minatku.databinding.ActivityNavbarBinding
+import com.bangkit.minatku.ui.community.CommunityFragment
 import com.bangkit.minatku.ui.home.HomeFragment
 import com.bangkit.minatku.ui.mentor.MentorFragment
 import com.bangkit.minatku.ui.modul.premium.ModulPremFragment
+import com.bangkit.minatku.ui.profil.ProfilFragment
 
 class NavbarActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNavbarBinding
@@ -20,6 +22,8 @@ class NavbarActivity : AppCompatActivity() {
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.NavHome -> replaceFragment(HomeFragment())
+                R.id.NavCommunity -> replaceFragment(CommunityFragment())
+                R.id.NavProfile -> replaceFragment(ProfilFragment())
                 R.id.NavModul -> replaceFragment(ModulPremFragment())
                 R.id.NavMentor -> replaceFragment(MentorFragment())
             }
