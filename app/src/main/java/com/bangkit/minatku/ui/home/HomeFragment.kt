58547b6f676d3edfb86654d22a2ae2c184fe8ceb
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-19 02:48:45 +0700

    Add JoinPremium Activity

--- app/src/main/java/com/bangkit/minatku/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/minatku/ui/home/HomeFragment.kt
@@ -1,17 +1,33 @@
 package com.bangkit.minatku.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.bangkit.minatku.R
+import com.bangkit.minatku.ui.joinpremium.JoinPremiumActivity
 
 class HomeFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        // Find the join_button by its ID
+        val joinButton: Button = view.findViewById(R.id.join_button)
+
+        // Set an OnClickListener for the join_button
+        joinButton.setOnClickListener {
+            // Launch the JoinPremiumActivity
+            val intent = Intent(activity, JoinPremiumActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
+    }
 }
--- app/src/main/java/com/bangkit/minatku/ui/mentor/MentorFragment.kt
+++ app/src/main/java/com/bangkit/minatku/ui/mentor/MentorFragment.kt
@@ -1,16 +1,33 @@
 package com.bangkit.minatku.ui.mentor
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.bangkit.minatku.R
+import com.bangkit.minatku.ui.joinpremium.JoinPremiumActivity
 
 class MentorFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mentor, container, false)
+        val view = inflater.inflate(R.layout.fragment_mentor, container, false)
+
+        // Find the join_button by its ID
+        val joinButton: Button = view.findViewById(R.id.join_button)
+
+        // Set an OnClickListener for the join_button
+        joinButton.setOnClickListener {
+            // Launch the JoinPremiumActivity
+            val intent = Intent(activity, JoinPremiumActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
