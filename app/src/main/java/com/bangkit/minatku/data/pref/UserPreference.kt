6badc0ee2442b9b9bb6e0de181b031fb9ddaa48b
Novallino Hamid Kiapmajaya <geraksiap1@gmail.com>
2023-12-20 18:12:23 +0700

    fix bug

--- app/src/main/java/com/bangkit/minatku/data/pref/UserPreference.kt
+++ app/src/main/java/com/bangkit/minatku/data/pref/UserPreference.kt
@@ -5,6 +5,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
@@ -20,6 +21,7 @@ class UserPreference private constructor(private val dataStore: DataStore<Prefer
             preferences[EMAIL_KEY] = user.email
             preferences[TOKEN_KEY] = user.token
             preferences[IS_LOGIN_KEY] = true
+            preferences[USER_ID] = user.userId.toString() // Convert to String
         }
     }
 
@@ -29,7 +31,8 @@ class UserPreference private constructor(private val dataStore: DataStore<Prefer
                 preferences[NAME_KEY] ?: "",
                 preferences[EMAIL_KEY] ?: "",
                 preferences[TOKEN_KEY] ?: "",
-                preferences[IS_LOGIN_KEY] ?: false
+                preferences[IS_LOGIN_KEY] ?: false,
+                preferences[USER_ID]?.toInt() ?: 0 // Convert to Int
             )
         }
     }
@@ -45,6 +48,7 @@ class UserPreference private constructor(private val dataStore: DataStore<Prefer
         private val EMAIL_KEY = stringPreferencesKey("email")
         private val TOKEN_KEY = stringPreferencesKey("token")
         private val IS_LOGIN_KEY = booleanPreferencesKey("isLogin")
+        private val USER_ID = stringPreferencesKey("userId") // Change to stringPreferencesKey
 
         fun getInstance(dataStore: DataStore<Preferences>): UserPreference {
             return UserPreference(dataStore)
